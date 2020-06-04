1c93bd1890987fb87a8b9384015299d747a308f1
Khyzhniak <aleck3@ukr.net>
2020-06-04 15:24:34 +0300

    Final version

---
+++ pages/event/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'desctiption',
+          name: 'description',
+          content: 'You create a new event in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
